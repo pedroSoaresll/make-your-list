75cc800cb5f3177bda72d66d0cecdf04151c3aea
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2022-07-09 17:44:44 -0300

    feat: service work caching

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client'
 import App from './app'
 import theme from './app/libraries/chakra-theme'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -15,6 +16,11 @@ root.render(
     </React.StrictMode>
   </>
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
