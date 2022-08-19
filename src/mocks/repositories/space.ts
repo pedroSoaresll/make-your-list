7ba85fafdee6751c7e11c6ed5c98640397da0826
Pedro Soares <pedro.soares@ifood.com.br>
2022-08-19 01:49:58 -0300

    feat: api mocked with msw

--- src/mocks/repositories/space.ts
+++ src/mocks/repositories/space.ts
@@ -1,8 +1,11 @@
-import { Space } from '../../infra'
+import { v4 as uuid } from 'uuid'
+
+import { List, Space } from '../../infra'
 
 export class SpaceRepository {
-  private SESSION_STORAGE_SPACE_KEY = 'spaces'
   spaces: Space[] = []
+
+  private SESSION_STORAGE_SPACE_KEY = 'spaces'
 
   constructor() {
     const spacesRaw = window.sessionStorage.getItem(
@@ -10,16 +13,12 @@ export class SpaceRepository {
     )
 
     if (spacesRaw) {
-      this.spaces = JSON.parse(spacesRaw)
+      this.overrideSpaces(JSON.parse(spacesRaw))
     }
   }
 
-  create(id: string, name: string) {
-    this.spaces.push({
-      id,
-      name,
-      lists: [],
-    })
+  private overrideSpaces(newSpace: Space[]) {
+    this.spaces = newSpace
 
     window.sessionStorage.setItem(
       this.SESSION_STORAGE_SPACE_KEY,
@@ -27,7 +26,64 @@ export class SpaceRepository {
     )
   }
 
-  get(id: string) {
-    return this.spaces.find((item) => item.id === id)
+  create(spaceId: string, name: string) {
+    const newSpace = { id: spaceId, name, lists: [] }
+    const spaces = [...this.spaces, newSpace]
+
+    this.overrideSpaces(spaces)
+  }
+
+  get(spaceId: string) {
+    return this.spaces.find((item) => item.id === spaceId)
+  }
+
+  createList(spaceId: string, list: List) {
+    const spaces = this.spaces.map((space) => {
+      if (space.id === spaceId) {
+        const dateNow = new Date().toISOString()
+
+        list.id = uuid()
+        list.spaceId = spaceId
+        list.createdAt = dateNow
+        list.updatedAt = dateNow
+
+        space.lists.push(list)
+      }
+
+      return space
+    })
+
+    this.overrideSpaces(spaces)
+  }
+
+  deleteList(spaceId: string, listId: string) {
+    const spaces = this.spaces.map((space) => {
+      if (space.id === spaceId) {
+        space.lists = space.lists.filter((list) => list.id !== listId)
+      }
+
+      return space
+    })
+
+    this.overrideSpaces(spaces)
+  }
+
+  patchList(spaceId: string, listId: string, body: List) {
+    const spaces = this.spaces.map((space) => {
+      if (space.id === spaceId) {
+        space.lists = space.lists.map((list) => {
+          if (list.id === listId) {
+            list.assigned = body.assigned
+            list.name = body.name
+          }
+
+          return list
+        })
+      }
+
+      return space
+    })
+
+    this.overrideSpaces(spaces)
   }
 }
