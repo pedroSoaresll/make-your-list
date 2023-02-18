2b93b86790a8217b9b8fb836149282fcc6456c99
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2023-02-18 13:44:42 -0300

    test: add tests to list Add module

---
+++ src/modules/lists/Add/index.spec.tsx
@@ -0,0 +1,61 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { useMutationCreateList } from '../../../infra/hooks/use-mutation-create-list'
+import { renderWithProviders } from '../../../tests/helpers/render-with-providers'
+import { Add } from '.'
+
+jest.mock('../../../infra/hooks/use-mutation-create-list')
+
+describe(`<Add />`, () => {
+  test('should do not allow submit button when form is invalid', async () => {
+    const spaceId = '44572e78-aeac-49f4-b74a-c918e40ebbaf'
+    const mutateFn = jest.fn()
+
+    ;(useMutationCreateList as jest.Mock).mockImplementation(() => ({
+      mutate: mutateFn,
+    }))
+
+    renderWithProviders(<Add spaceId={spaceId} />)
+
+    // open modal
+    await userEvent.click(
+      screen.getByRole('button', { name: 'Adicionar presente' })
+    )
+
+    // click into submit button
+    await userEvent.click(screen.getByRole('button', { name: 'Adicionar' }))
+
+    expect(mutateFn).not.toHaveBeenCalled()
+    expect(screen.getByText('Preencha o nome.')).toBeInTheDocument()
+  })
+
+  test('should submit the form', async () => {
+    const spaceId = '44572e78-aeac-49f4-b74a-c918e40ebbaf'
+    const itemName = 'Novo item'
+    const mutateFn = jest.fn()
+
+    ;(useMutationCreateList as jest.Mock).mockImplementation(() => ({
+      mutate: mutateFn,
+    }))
+
+    renderWithProviders(<Add spaceId={spaceId} />)
+
+    // open modal
+    await userEvent.click(
+      screen.getByRole('button', { name: 'Adicionar presente' })
+    )
+
+    // write into input
+    await userEvent.type(
+      screen.getByRole('textbox', { name: 'Nome:' }),
+      itemName
+    )
+
+    // click into submit button
+    await userEvent.click(screen.getByRole('button', { name: 'Adicionar' }))
+
+    expect(mutateFn).toHaveBeenCalledTimes(1)
+    expect(mutateFn).toHaveBeenCalledWith({ spaceId, name: itemName })
+  })
+})
