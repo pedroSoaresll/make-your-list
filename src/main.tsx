199c8cc2017e40d3a23ffb5d128c6caae2303669
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2024-08-27 19:23:15 -0300

    fix: vitest build

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
+import './reportWebVitals'
+
 import { ColorModeScript } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './app'
 import theme from './app/libraries/chakra-theme'
-import reportWebVitals from './reportWebVitals'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
-if (import.meta.env.VITE_USE_API_MOCKED === 'true') {
-  const { worker } = await import('./mocks/browser')
+const loadMockServer = async () => {
+  if (import.meta.env.VITE_USE_API_MOCKED === 'true') {
+    const { worker } = await import('./mocks/browser')
 
-  worker.start()
+    worker.start()
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
@@ -28,7 +31,4 @@ root.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+loadMockServer()
